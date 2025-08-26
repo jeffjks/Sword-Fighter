1679b18918eb9fbe569fe5f131c223d806852320
KunSang.Cho <jeff_jks@naver.com>
2025-08-26 19:36:28 +0900

    서버측 SkillData Handle 추가

--- Server/SwordFighterServer/SwordFighterServer/SkillDatabase.cs
+++ Server/SwordFighterServer/SwordFighterServer/SkillDatabase.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Shared.Enums;
 using System.IO;
 
 namespace SwordFighterServer
 {
+    public struct SkillData
+    {
+        public string skillName;
+        public float duration;
+        public float cooldown;
+    }
+
     public class SkillDatabase
     {
         private static JObject _skillData;
 
         private const string JsonPath = @"C:\Program Files\Unity\Project\Sword-Fighter\Assets\ExportedSkill.json";
+        private const string SkillName = "skillName";
+        private const string Duration = "duration";
+        private const string Cooldown = "cooldown";
         private const string ModuleName = "modules";
 
         public SkillDatabase()
@@ -29,6 +38,18 @@ namespace SwordFighterServer
             {
                 SkillEffectProcessor.Apply(caster, skillInput, skillEffectModule);
             }
+        }
+
+        public static SkillData GetSkillData(PlayerSkill playerSkill)
+        {
+            var skill = _skillData[playerSkill.ToString()];
+            var skillName = (string)skill[SkillName];
+            var duration = (float)skill[Duration];
+            var cooldown = (float)skill[Cooldown];
+
+            SkillData skillData = new SkillData { skillName = skillName, duration = duration, cooldown = cooldown };
+
+            return skillData;
         }
     }
 }
